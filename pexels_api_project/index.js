32e7290fed6a5cc5ec2160aaf2517629342880f4
Yuliia Hlamazdenko <glammys1995@gmail.com>
2022-08-17 12:02:40 -0400

    fixed search-bar keyup issue, window onload

--- pexels_api_project/index.js
+++ pexels_api_project/index.js
@@ -1,27 +1,23 @@
-window.onload=function(){
-    var searchForm = document.getElementById(`search-form`)
-    searchForm.addEventListener('submit', function(e) {
-        e.preventDefault();
+var searchForm = document.getElementById(`search-form`)
+searchForm.addEventListener('submit', function(e) {
+    e.preventDefault();
 
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                const obj = JSON.parse(xhttp.responseText);
-                document.querySelector(`.search-result-container`).innerHTML = " ";
-                for(let photo of obj.photos) {
-                    $(`.search-result-container`).append(`<div class="search-result-item"><img src=${photo.src.landscape}><h4>${photo.photographer}</h4></div>`);
-                };
-            }
-        };
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            const obj = JSON.parse(xhttp.responseText);
+            document.querySelector(`.search-result-container`).innerHTML = " ";
+            for(let photo of obj.photos) {
+                $(`.search-result-container`).append(`<div class="search-result-item"><img src=${photo.src.landscape}><h4>${photo.photographer}</h4></div>`);
+            };
+        }
+    };
 
-        $("#search-bar").keyup(function() {
-            const value = $(this).val();
-            document.querySelector(`#results-name`).innerHTML = " ";
-            xhttp.open("GET", `https://api.pexels.com/v1/search/?page=1&per_page=12&query=${value}`, true);
-            xhttp.setRequestHeader('Authorization', '563492ad6f91700001000001a7ba1db44ff44af29744cadd59a69656');
-            xhttp.send();
-            $(`#results-name`).append(`<h2>${value[0].toUpperCase().concat(value.slice(1))} Images</h2>`)
-        });
-    });
-};
+    const value = $(`#search-bar`).val();
+    document.querySelector(`#results-name`).innerHTML = " ";
+    xhttp.open("GET", `https://api.pexels.com/v1/search/?page=1&per_page=12&query=${value}`, true);
+    xhttp.setRequestHeader('Authorization', '563492ad6f91700001000001a7ba1db44ff44af29744cadd59a69656');
+    xhttp.send();
+    $(`#results-name`).append(`<h2>${value[0].toUpperCase().concat(value.slice(1))} Images</h2>`)
+});
 
